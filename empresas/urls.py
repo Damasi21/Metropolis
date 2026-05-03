2a05548c568f514263d40a8bc169b645b93d75d8
Marcelo Damasi <marcelo@md21.com.br>
2026-05-03 00:01:16 -0300

    03-05 login

--- empresas/urls.py
+++ empresas/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
-from .views import configuracoes_empresas
+from .views import configuracoes_empresas, excluir_usuario_empresa, usuarios_empresa
 
 urlpatterns = [
     path('configuracoes/', configuracoes_empresas, name='configuracoes_empresas'),
+    path('usuarios/', usuarios_empresa, name='usuarios_empresa'),
+    path('usuarios/<int:usuario_id>/editar/', usuarios_empresa, name='editar_usuario_empresa'),
+    path('usuarios/<int:usuario_id>/excluir/', excluir_usuario_empresa, name='excluir_usuario_empresa'),
 ]
