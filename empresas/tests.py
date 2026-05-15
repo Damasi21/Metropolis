0ede228c33a0a3a6adfeb48f1620730fa53bef8c
Marcelo Damasi <marcelo@md21.com.br>
2026-05-15 14:14:37 -0300

    15-05

--- empresas/tests.py
+++ empresas/tests.py
@@ -1,3 +1,26 @@
 from django.test import TestCase
+from django.urls import reverse
 
-# Create your tests here.
+from dashboards.models import ParametroEmpresa
+from .models import Empresa
+
+
+class ConfiguracoesEmpresasTests(TestCase):
+    def test_cadastro_empresa_cria_parametros_do_dashboard(self):
+        response = self.client.post(
+            reverse('configuracoes_empresas'),
+            {
+                'nome': 'Empresa Nova',
+                'slug': 'empresa-nova',
+                'ativo': 'on',
+            },
+        )
+
+        self.assertRedirects(response, reverse('configuracoes_empresas'))
+        self.assertTrue(Empresa.objects.filter(slug='empresa-nova').exists())
+        self.assertTrue(
+            ParametroEmpresa.objects.filter(
+                slug_empresa='empresa-nova',
+                nome_empresa='Empresa Nova',
+            ).exists()
+        )
